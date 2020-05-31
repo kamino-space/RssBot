244099a4e89e919d9edc31ef3aeff3f14ebc398d
kamino-space <lishichenjs@163.com>
2020-05-31 21:48:28 +0800

    k收信人为空不发生

--- rssbot/bot.py
+++ rssbot/bot.py
@@ -10,9 +10,13 @@ class QQBot(object):
         self.bot = CQHttp(api_root=cqapi)
 
     def send_private_msg(self, user_id, message=''):
+        if user_id is None:
+            return
         logging.info('向[%s]发送私聊消息: %s' % (user_id, message[0:20]))
         self.bot.send_private_msg(user_id=user_id, message=message)
 
     def send_group_msg(self, group_id, message=''):
+        if group_id is None:
+            return
         logging.info('向[%s]发送群组消息: %s' % (group_id, message[0:20]))
         self.bot.send_group_msg(group_id=group_id, message=message)
